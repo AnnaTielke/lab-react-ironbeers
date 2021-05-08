7e9e8921719d0cbdc7e26bdd8e89ad43d778c857
Anna Tielke <anna_tielke@gmx.de>
2021-05-08 13:56:54 +0200

    Iteration 1, created the homepage, used bootstrap, linked it

---
+++ src/components/Beers.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Beers extends Component {
+  render() {
+    return <div>test test</div>;
+  }
+}
+
+export default Beers;
---
+++ src/components/Home.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import beers from '../assets/beers.png';
+import newbeer from '../assets/new-beer.png';
+import randombeer from '../assets/random-beer.png';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div className="card mb-3">
+          <img src={beers} class="card-img-top" alt="..." />
+          <div className="card-body">
+            <Link to={`/beers`}>
+              <h1 className="card-title">All Beers</h1>
+            </Link>
+            <p className="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+        </div>
+
+        <div className="card mb-3">
+          <img src={randombeer} class="card-img-top" alt="..." />
+          <div className="card-body">
+            <Link to={`/random-beer`}>
+              <h1 className="card-title">Random Beer</h1>
+            </Link>
+            <p className="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+        </div>
+
+        <div className="card mb-3">
+          <img src={newbeer} class="card-img-top" alt="..." />
+          <div className="card-body">
+            <Link to={`/new-beer`}>
+              <h1 className="card-title">New Beer</h1>
+            </Link>
+            <p className="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
