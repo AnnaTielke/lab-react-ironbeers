333a96d0d61f91d2d83622ec158e8304796050bd
Anna Tielke <anna_tielke@gmx.de>
2021-05-09 15:09:36 +0200

    displayed the beer details, Iteration 4

--- src/components/BeerDetail.js
+++ src/components/BeerDetail.js
@@ -21,7 +21,34 @@ class BeerDetail extends Component {
   }
 
   render() {
-    return <div></div>;
+    console.log('look at the state');
+    console.log(this.state.beer);
+    const {
+      image_url,
+      name,
+      tagline,
+      first_brewed,
+      attenuation_level,
+      description,
+      contributed_by,
+    } = this.state.beer;
+    return (
+      <div>
+        <div className="card" style={{ width: '18rem' }}>
+          <img src={image_url} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{name}</h5>
+            <p>{attenuation_level}</p>
+            <p className="card-text">{tagline}</p>
+            <p>{first_brewed}</p>
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">{description}</li>
+            <li className="list-group-item">{contributed_by}</li>
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
 
